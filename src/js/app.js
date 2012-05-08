e0323a8a7a27da7dbf543d14452c6284d3b27780
pazguille <guille87paz@gmail.com>
2012-05-08 11:35:02 -0300

    Add new files

---
+++ src/js/app.js
@@ -0,0 +1,132 @@
+/*
+* Models
+*/
+
+var Photo = Backbone.Model.extend({});
+
+/*
+* Collections
+*/
+var PhotoCollection = Backbone.Collection.extend({
+	"model": Photo,
+
+	"sync": function (method, model, options) {
+		options.dataType = "jsonp";
+		options.data = {"client_id": "e7f1bd84f9c34c02a3bc88cd058942a1"};
+
+		return Backbone.sync(method, model, options);
+	},
+
+	"parse": function (response) {
+		return response.data;
+	},
+
+	"url": "https://api.instagram.com/v1/media/popular"
+});
+
+/*
+* Views
+*/
+var PhotoView = Backbone.View.extend({
+	"tagName": "li",
+
+	"template": _.template($("#tpl-photo").html()),
+
+	"render": function () {
+		var photo = this.model,
+			caption = photo.get("caption"),
+			description = "";
+
+		if (caption !== null) {
+			description = photo.get("caption").text;
+		}
+
+		photo.set("description", description)
+
+		photo = photo.toJSON();
+
+		$(this.el).html(this.template(photo));
+
+		return this;
+	}
+});
+
+var AppView = Backbone.View.extend({
+	"el": "#hottest",
+
+	"initialize": function () {
+		this.collection = new PhotoCollection();
+		
+		this.$el
+			.prepend(this.$list);
+
+		this.$el.removeClass("ch-hide");
+
+		this.reset();
+
+		this.fetch();
+	},
+
+	"events": {
+		"scroll": "more",
+		"click .repin": "repin"
+	},
+
+	"$list": $("<ul class=\"ch-slats ch-hide\">"),
+
+	"$loading": $(".ch-loading"),
+
+	"render": function () {
+		var that = this;
+
+		_.each(this.collection.models, function (photo) {
+			var photo = new PhotoView({"model": photo});
+			that.$list.append(photo.render().el);
+		}, this);
+
+		this.$list.removeClass("ch-hide");
+
+		that.trigger("end");
+
+		return this;
+	},
+
+	"fetch":  function () {
+		var that  = this;
+		this.$loading.removeClass("ch-hide");
+		this.collection.fetch({
+			"success": function () {
+				that.$loading.addClass("ch-hide");
+				that.render();
+			}
+		});
+	},
+
+	"more": function () {
+		var height = this.$list.height() - this.$el.height();
+		var bottom = this.el.scrollTop;
+		if (height === bottom) {
+			this.fetch();
+		};
+
+		return;
+	},
+
+	"repin": function (event) {		
+		chrome.tabs.create({url: event.target.href});
+		window.close();
+
+		return false;
+	},
+
+	"reset": function () {
+		this.collection.reset();
+		this.$list.html("");
+	}
+
+});
+
+var hottest;
+setTimeout(function () {
+	hottest = new AppView();
+}, 1000);
